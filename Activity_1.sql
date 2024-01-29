3fdeeefd07f3a714dd84470c7fa78fb561206feb
TJ Maurea Dhanniela <147401825+dhanniela@users.noreply.github.com>
2024-01-29 20:44:59 +0800

    fix code

--- Activity_1.sql
+++ Activity_1.sql
@@ -9,12 +9,12 @@ CREATE TABLE students (
 );
 
 INSERT INTO students (id, first_name, middle_name, last_name, age, location) VALUES
-(1, 'Juan', '', 'Cruz', 18, 'Manila'),
-(2, 'Anne', '', 'Wall', 20, 'Manila'),
-(3, 'Theresa', '', 'Joseph', 21, 'Manila'),
-(4, 'Issac', '', 'Gray', 19, 'Laguna'),
-(5, 'Zack', '', 'Matthews', 22, 'Marikina'),
-(6, 'Finn', '', 'Lam', 25, 'Manila');
+(1, 'Juan', 'Blank', 'Cruz', 18, 'Manila'),
+(2, 'Anne', 'Blank', 'Wall', 20, 'Manila'),
+(3, 'Theresa', 'Blank', 'Joseph', 21, 'Manila'),
+(4, 'Issac', 'Blank', 'Gray', 19, 'Laguna'),
+(5, 'Zack', 'Blank', 'Matthews', 22, 'Marikina'),
+(6, 'Finn', 'Blank', 'Lam', 25, 'Manila');
 
 UPDATE students
 SET first_name = 'Ivan',
